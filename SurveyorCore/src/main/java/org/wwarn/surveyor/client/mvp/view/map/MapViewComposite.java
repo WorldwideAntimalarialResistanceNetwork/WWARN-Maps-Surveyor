7bcb8c7b979f10b95ab2b1e014f4f5df6bd37035
Ignacio Suay <ignacio.suay@gmail.com>
2015-03-19 11:50:10 +0000

    Add the possibility to config the map type between Hybrid, Terrain, Satellite, and RoadMap....

--- SurveyorCore/src/main/java/org/wwarn/surveyor/client/mvp/view/map/MapViewComposite.java
+++ SurveyorCore/src/main/java/org/wwarn/surveyor/client/mvp/view/map/MapViewComposite.java
@@ -121,7 +121,7 @@ public class MapViewComposite extends Composite {
             public void execute() {
                 MapBuilder builder = new MapBuilder();
                 //todo move type of map into config
-                mapWidget = builder.configureMapDimension(400, 500).setCenter(viewConfig.getInitialLat(), viewConfig.getInitialLon()).setZoomLevel(viewConfig.getInitialZoomLevel()).createMapWidget(MapBuilder.MapType.GOOGLE_V3);
+                mapWidget = builder.configureMapDimension(400, 500).setCenter(viewConfig.getInitialLat(), viewConfig.getInitialLon()).setZoomLevel(viewConfig.getInitialZoomLevel()).setMapTypeId(viewConfig.getMapType()).createMapWidget(MapBuilder.MapType.GOOGLE_V3);
                 setMarkers(clientFactory.getLastQueryResult());
                 panel.add(mapWidget);
                 mapWidget.justResizeMapWidget();
