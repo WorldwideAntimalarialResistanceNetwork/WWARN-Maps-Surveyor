9f7ad9908af8751fe0f73b6dbbede54086c677a5
Ignacio Suay <ignacio.suay@gmail.com>
2016-02-12 16:30:52 +0000

    remove setinitial range to avoid load data 2 times at the beginning

--- SurveyorCore/src/main/java/org/wwarn/surveyor/client/mvp/view/filter/YearRangeSliderComposite.java
+++ SurveyorCore/src/main/java/org/wwarn/surveyor/client/mvp/view/filter/YearRangeSliderComposite.java
@@ -8,18 +8,18 @@ package org.wwarn.surveyor.client.mvp.view.filter;
  * %%
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. Neither the name of the University of Oxford nor the names of its contributors
  *    may be used to endorse or promote products derived from this software without
  *    specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -200,7 +200,7 @@ public class YearRangeSliderComposite extends Composite{
 
     private YuiDualSliderGwtWidget setupYearRangeWidget() {
         YuiDualSliderGwtWidget dateSliderYuiWidgetImpl = new YuiDualSliderGwtWidgetImplWithLabelMarkers(456, 48, startYear, Integer.valueOf(endYear), 5, 1);
-        dateSliderYuiWidgetImpl.setRange(initialStart, initialEnd);
+        //dateSliderYuiWidgetImpl.setRange(initialStart, initialEnd);
         ValueChangeHandler<Range<Integer>> valueChangeHandler = new ValueChangeHandler<Range<Integer>>() {
 
             public void onValueChange(ValueChangeEvent<Range<Integer>> yearRangeValueChangeEvent) {
