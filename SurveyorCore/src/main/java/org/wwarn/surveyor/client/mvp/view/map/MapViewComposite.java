45abed44cca64d1db2a68f4860357071be27221a
Ignacio Suay <ignacio.suay@gmail.com>
2016-09-07 14:06:45 +0100

    Create a centerMapEvent and handle it in the map

--- SurveyorCore/src/main/java/org/wwarn/surveyor/client/mvp/view/map/MapViewComposite.java
+++ SurveyorCore/src/main/java/org/wwarn/surveyor/client/mvp/view/map/MapViewComposite.java
@@ -8,18 +8,18 @@ package org.wwarn.surveyor.client.mvp.view.map;
  * %%
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. Neither the name of the University of Oxford nor the names of its contributors
  *    may be used to endorse or promote products derived from this software without
  *    specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -45,13 +45,10 @@ import org.wwarn.mapcore.client.components.customwidgets.LegendButton;
 import org.wwarn.surveyor.client.core.DataSchema;
 import org.wwarn.surveyor.client.core.QueryResult;
 import org.wwarn.surveyor.client.core.RecordList;
-import org.wwarn.surveyor.client.event.InterfaceLoadCompleteEvent;
-import org.wwarn.surveyor.client.event.ToggleLayerEvent;
+import org.wwarn.surveyor.client.event.*;
 import org.wwarn.surveyor.client.model.MapViewConfig;
 import org.wwarn.mapcore.client.utils.StringUtils;
 import org.wwarn.surveyor.client.mvp.ClientFactory;
-import org.wwarn.surveyor.client.event.MarkerClickEvent;
-import org.wwarn.surveyor.client.event.ResultChangedEvent;
 import org.wwarn.surveyor.client.mvp.SimpleClientFactory;
 
 import java.util.ArrayList;
@@ -268,6 +265,12 @@ public class MapViewComposite extends Composite {
             final OfflineMapWidget offlineMapWidget = (OfflineMapWidget) ((OfflineMapWidget) mapWidget);
             offlineMapWidget.toggleLayer(toggleLayerEvent.getLayerName());
         }
+    }
+
+    @EventHandler
+    public void onCenterMap(CenterMapEvent centerMapEvent){
+        mapWidget.setCenter(centerMapEvent.getCoordinatesLatLon());
+        mapWidget.setZoomLevel(centerMapEvent.getZoomLevel());
     }
 
     private MarkerLegendLoader getMarkerLegendBuilder() {
